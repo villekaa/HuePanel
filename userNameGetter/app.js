285e8917924396f722ce35d531c10d38fb72720c
Goffy <42999143+villekaa@users.noreply.github.com>
2019-10-04 12:05:26 +0300

    Update app.js

--- userNameGetter/app.js
+++ userNameGetter/app.js
@@ -7,4 +7,4 @@ const url = 'http://192.168.0.102'
 axios.post(`${url}/api`, {"devicetype":"HueWebApp#Goffy"})
 .then(({ data }) => console.log(data))
 
-// [ { success: { username: 'cB75Wsi4QJHaA2zq4nJyvbjZVuS1-YK0FkkL8Gtr' } } ]
+// [ { success: { username: '' } } ]
